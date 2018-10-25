684996d76b137937212a0aaa50b3b3256ee8da5d
Artem2710 <you@example.com>
2018-10-26 01:02:47 +0300

    new version

--- AbstractActors.php
+++ AbstractActors.php
@@ -1,23 +1,7 @@
 <?php 
-
 abstract class AbstractActors
 {
-	private $_db = null;
-
-	public function __construct()
-	{
-		$this->_db = DB::getInstance();
-	}
-
-	public function getSomething($sql)
-	{
-		$result = $this->_db->query($sql);
-		return $result->fetchAll(PDO::FETCH_ASSOC);
-	}
 	
-    public function insertSomething($sql)
-	{
-	    $this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    $this->_db->exec($sql);
-	}
+    abstract public function getSomething();
+    abstract public function insertSomething();
 }
--- Actors.php
+++ Actors.php
@@ -5,13 +5,33 @@ require_once 'DB.php';
 
 class Actors extends AbstractActors
 {
+	private $_db = null;
+	public static $tableName = __CLASS__;
 
+	public function __construct()
+	{
+		$this->_db = DB::getInstance();
+	}
+
+	public function getSomething()
+	{
+		$sql = "SELECT full_name from " . static::$tableName . " WHERE left(full_name, 1) in (select distinct gender from actors) order by full_name desc";
+		$result = $this->_db->query($sql);
+		return $result->fetchAll(PDO::FETCH_ASSOC);
+	}
+	
+	public function insertSomething()
+	{
+		$sql = "INSERT into " . static::$tableName . " (full_name, gender) select any_actors.full_name, any_actors.gender from any_actors";
+	    $this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    $this->_db->exec($sql);
+	}
 }
 
 $actors = new Actors();
 
 echo "<pre>";
-var_dump($actors->getSomething("SELECT full_name from actors where left(full_name, 1) in (select distinct gender from actors) order by full_name desc"));
+var_dump($actors->getSomething());
 echo "<pre>";
 
-$actors->insertSomething("INSERT into actors (full_name, gender) select any_actors.full_name, any_actors.gender from any_actors");
+$actors->insertSomething();
